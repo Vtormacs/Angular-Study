b2035547bf58cf66333708f7eeaf6314103a3508
Vtormacs <macskids98play@hotmail.com>
2024-10-11 18:12:48 -0300

    aula 2, criacao de componente parte 2

--- Componente/src/app/app.module.ts
+++ Componente/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { Card2Component } from './card2/card2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    Card2Component
   ],
   imports: [
     BrowserModule,
