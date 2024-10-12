3ad20255f11e993cdaa1152567c714037ae29c39
Vtormacs <macskids98play@hotmail.com>
2024-10-12 12:32:03 -0300

    Componente filho

--- Componente/src/app/app.module.ts
+++ Componente/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { Card2Component } from './card2/card2.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { Card2ButtonComponent } from './card2-button/card2-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    Card2Component
+    Card2Component,
+    CardButtonComponent,
+    Card2ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ Componente/src/app/card2-button/card2-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card2-button',
+  templateUrl: './card2-button.component.html',
+  styleUrl: './card2-button.component.scss'
+})
+export class Card2ButtonComponent {
+
+}
