46182fde42e4a342ecd0c4ee435e5909512472d1
Vtormacs <macskids98play@hotmail.com>
2024-10-17 14:41:09 -0300

    O que é Property Binding

--- Binding-Diretivas-Templates-Decorators/src/app/app.component.ts
+++ Binding-Diretivas-Templates-Decorators/src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Binding-Diretivas-Templates-Decorators';
+  inputText = 'Texto Inicial';
+  inputType = 'text';
+  isDisabled = false;
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+
+  disableInput() {
+    this.isDisabled = true;
+  }
+
+  textValueInput() {
+    this.inputText = 'Texto Alterado';
+  }
+
+  textOriginal() {
+    this.inputText = 'Texto Inicial';
+  }
+
+  mudarTypePasswor() {
+    this.inputType = 'password';
+  }
+
+  mudarTypeText() {
+    this.inputType = 'text';
+  }
+
+  logInputText() {
+    console.log(this.inputText);
+  }
 }
