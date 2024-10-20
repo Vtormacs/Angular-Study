2ebb1b697a69d20ecb5c326ee1efb2ec0cab2db1
Vtormacs <macskids98play@hotmail.com>
2024-10-20 15:06:35 -0300

    seila

---
+++ aula/src/app/components/eventos-list/eventos-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-eventos-list',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './eventos-list.component.html',
+  styleUrl: './eventos-list.component.scss'
+})
+export class EventosListComponent {
+
+}
