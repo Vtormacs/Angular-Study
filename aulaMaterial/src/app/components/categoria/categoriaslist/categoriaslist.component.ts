05f65357c97a2f489a44cedd4d01913fa0379f81
Vtormacs <macskids98play@hotmail.com>
2024-10-22 18:25:14 -0300

    material

---
+++ aulaMaterial/src/app/components/categoria/categoriaslist/categoriaslist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoriaslist',
+  standalone: true,
+  imports: [],
+  templateUrl: './categoriaslist.component.html',
+  styleUrl: './categoriaslist.component.scss'
+})
+export class CategoriaslistComponent {
+
+}
---
+++ aulaMaterial/src/app/models/endereco.ts
@@ -0,0 +1,7 @@
+export class Endereco {
+  id!: number;
+  estado!: string;
+  cidade!: string;
+  rua!: string;
+  numero!: string;
+}
