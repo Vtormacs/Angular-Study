a451dcb95079e8fed39df0cfd6e12c67b9b8dc26
Vtormacs <macskids98play@hotmail.com>
2024-10-23 16:32:36 -0300

    navbar e rodape

--- aulaMaterial/src/app/components/layout/principal/principal.component.ts
+++ aulaMaterial/src/app/components/layout/principal/principal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../menu/menu.component";
 import { RouterOutlet } from '@angular/router';
+import { RodapeComponent } from "../../rodape/rodape/rodape.component";
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [MenuComponent, RouterOutlet],
+  imports: [MenuComponent, RouterOutlet, RodapeComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
