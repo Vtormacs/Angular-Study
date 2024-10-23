46ca6ebe4b2d8618140f13a10332d77f2f9fbca9
Vtormacs <macskids98play@hotmail.com>
2024-10-23 17:45:54 -0300

    login

--- aulaMaterial/src/app/components/layout/login/login.component.ts
+++ aulaMaterial/src/app/components/layout/login/login.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MdbFormsModule],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email!: string;
+  senha!: string;
+
+  router = inject(Router);
+
+  logar() {
+    if (this.email == 'admin@gmail.com' && this.senha == 'admin') {
+      this.router.navigate(['admin/eventos']);
+    } else {
+      alert('Dados incorretos');
+    }
+  }
+}
